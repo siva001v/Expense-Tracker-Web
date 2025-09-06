a08a337955a380ed41d375aa88c60092774ff89e
siva001v <sivasankar001v@gmail.com>
2025-09-06 21:46:17 +0530

    Bug fix on api constants

--- api/api-constants.js
+++ api/api-constants.js
@@ -1,7 +1,6 @@
-export const API_BASE_URL = "https://api.example.com";
+export const API_BASE_URL = "https://expense-tracker-server-mwa5.onrender.com";
 
 export const API_ENDPOINTS = {
-  BASEURL: "https://expense-tracker-server-mwa5.onrender.com",
   GETEXPENSES: "/api/expenses",
   GETTRENDS: "/api/expenses/trends",
   GETSUMMARY: "/api/expenses/summary",
